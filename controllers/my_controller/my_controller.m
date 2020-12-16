b4869b64cd41bf4a4d6204320ab322fd763225f6
Mexopo <73990220+Mexopo@users.noreply.github.com>
2020-12-16 21:13:30 +0100

    Svet_vytvorený_nedokončený

--- controllers/my_controller/my_controller.m
+++ controllers/my_controller/my_controller.m
@@ -21,7 +21,34 @@ TIME_STEP = 64;
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
 %
+motor_1 = wb_robot_get_device('forearm');
+% wb_motor_set_position(motor_1, 1.5)
+motor_2 = wb_robot_get_device('wrist');
+wb_motor_set_position(motor_2, -0.8)
+motor_3 = wb_robot_get_device('upperarm');
+wb_motor_set_position(motor_3, -1.5)
+gripper_l = wb_robot_get_device('left_gripper');
+wb_motor_set_position(gripper_l, -1)
+gripper_r = wb_robot_get_device('right_gripper');
+wb_motor_set_position(gripper_r, 1)
+motor_4 = wb_robot_get_device('rotational_wrist');
+wb_motor_set_position(motor_4, -3)
+motor_5 = wb_robot_get_device('base');
+
+dis_senzor_1 = wb_robot_get_device('ds4');
+wb_distance_sensor_enable(dis_senzor_1, TIME_STEP)
+dis_senzor_2 = wb_robot_get_device('pozicia_kocky');
+wb_distance_sensor_enable(dis_senzor_2, TIME_STEP)
+
 while wb_robot_step(TIME_STEP) ~= -1
+distance = wb_distance_sensor_get_value(dis_senzor_2)
+if distance > 0.8
+wb_motor_set_position(gripper_r, 0.2)
+wb_motor_set_position(gripper_l, -0.2)
+pause(1, TIME_STEP)
+wb_motor_set_position(motor_3, 0)
+wb_motor_set_position(motor_5, 5)
+end
 
   % read the sensors, e.g.:
   %  rgb = wb_camera_get_image(camera);
